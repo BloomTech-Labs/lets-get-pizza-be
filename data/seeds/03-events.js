3c60dcad49ed291f2d64d8bc541316eb21bb29e7
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-04-20 20:05:59 -0700

    ability to find events by user id

--- data/seeds/03-events.js
+++ data/seeds/03-events.js
@@ -1,28 +1,37 @@
-exports.seed = function(knex, Promise) {
-      return knex('events').insert([
-        {
-          user_id: 1,
-          location_id: 1,
-          title: 'Lets Get Pizza!',
-          description: 'Its dat time boys, pizza!',
-          start_time: '1/20/2020',
-          end_time: '1/22/2020'
-        },
-        {
-            user_id: 1,
-            location_id: 1,
-            title: 'Nacho Pizza Hunt!',
-            description: 'Are Nacho pizzas a thing? Lets find out!',
-            start_time: '1/20/2020',
-            end_time: '1/22/2020'
-        },
-        {
-            user_id: 1,
-            location_id: 2,
-            title: 'The End of Days',
-            description: 'About that time, lets have a slice to celebrate the end of the world.',
-            start_time: '1/20/2020',
-            end_time: '1/22/2020'
-        }
-      ]);
-  };
+exports.seed = function (knex, Promise) {
+  return knex("events").insert([
+    {
+      user_id: 1,
+      location_id: 1,
+      title: "Lets Get Pizza!",
+      description: "Its dat time boys, pizza!",
+      start_time: "1/20/2020",
+      end_time: "1/22/2020",
+    },
+    {
+      user_id: 1,
+      location_id: 1,
+      title: "Nacho Pizza Hunt!",
+      description: "Are Nacho pizzas a thing? Lets find out!",
+      start_time: "1/20/2020",
+      end_time: "1/22/2020",
+    },
+    {
+      user_id: 1,
+      location_id: 2,
+      title: "The End of Days",
+      description:
+        "About that time, lets have a slice to celebrate the end of the world.",
+      start_time: "1/20/2020",
+      end_time: "1/22/2020",
+    },
+    {
+      user_id: 2,
+      location_id: 2,
+      title: "coronavirus meetup",
+      description: "I need a group of ten or more to meet up with me",
+      start_time: "3/15/2020",
+      end_time: "3/20/2020",
+    },
+  ]);
+};
