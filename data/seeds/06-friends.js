677e8870fc5b1f98dfbbc12c0b8cb14993c8cb54
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-04-21 21:09:06 -0700

    added join and fixed seeds

--- data/seeds/06-friends.js
+++ data/seeds/06-friends.js
@@ -1,7 +1,7 @@
 exports.seed = function (knex) {
   return knex("friends").insert([
-    { id: 1, user_id: 1, friends_id: 2, status: "accepted" },
-    { id: 2, user_id: 2, friends_id: 3, status: "accepted" },
-    { id: 3, user_id: 3, friends_id: 1, status: "requested" },
+    { id: 1, user_id: 3, friends_id: 2, status: "accepted" },
+    { id: 2, user_id: 3, friends_id: 1, status: "accepted" },
+    { id: 3, user_id: 2, friends_id: 3, status: "requested" },
   ]);
 };
