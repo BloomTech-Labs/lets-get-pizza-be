b17fe743fa1cb4d20e515c6843dbe9860a3ca294
Buddy Agyin <agyin3@gmail.com>
2020-04-22 23:45:12 -0600

    add tests for update and remove models

--- tests/components/promotions.js
+++ tests/components/promotions.js
@@ -6,7 +6,9 @@ const Promotions = require('../../components/promotions/promotions-model')
 // Promotions Dummy Data 
 const good_promotion_details = {location_id: 1, title: "COVID-19 Pizza Party", text: "Social Diastance with a pizza pie", start_date: "4/16/2020", end_date: "5/18/2020"}
 const bad_promotion_details = {location_id: 6, title: null, text: null, start_date: "4/20/2020"}
+const updated_promotion_details = {title: "NFL Draft Party", text: "25% of pizza when your team is drafting"}
 const match_promotion_details = {location_id: 1, title: "COVID-19 Pizza Party", text: "Social Diastance with a pizza pie"}
+
 
 // Locations Register Dummy Data
 const location_creds = {business_name: "testlocation", email: "testlocation@location.com", password: "test", latitude: 29.201598971549644, longitude: -98.66165965560205, address: "123 Test Drive"}
@@ -58,6 +60,58 @@ describe("Promotions Tests", () => {
 
             expect(returnedPromotions).toHaveLength(1)
             expect(returnedPromotions[0]).toMatchObject(match_promotion_details)
+        })
+    })
+
+    describe("Update Model", () => {
+        beforeEach(async () => {
+            await Promotions.add(good_promotion_details)
+        })
+
+        it("should return an error when no id or wrong provided", async () => {
+            const id = 24
+            const failedMatch = {message: 'Undefined binding(s) detected when compiling FIRST. Undefined column(s): [id] query: select * from "promotions" where "id" = ? limit ?'}
+
+            const failedUpdateId = await Promotions.update(updated_promotion_details, id)
+
+            expect(failedUpdateId).toMatchObject(failedMatch)
+        })
+
+        it("should return error with no id provided", async () => {
+            const failedMatch = {message: 'Undefined binding(s) detected when compiling UPDATE. Undefined column(s): [id] query: update "promotions" set "title" = ?, "text" = ? where "id" = ? returning "id"'}
+
+            const failedUpdateNoId = await Promotions.update(updated_promotion_details)
+
+            expect(failedUpdateNoId).toMatchObject(failedMatch)
+        })
+
+        it("should update promotion and return updated promotion", async () => {
+            const id = 1
+            const successfulUpdate = await Promotions.update(updated_promotion_details, id)
+
+            expect(successfulUpdate).toMatchObject(updated_promotion_details)
+        })
+    })
+
+    describe("Remove Model", () => {
+        beforeEach(async () => {
+            await Promotions.add(good_promotion_details)
+        })
+
+        it("should return error when passed wrong id", async () => {
+            const id = 55
+            const returned = await Promotions.remove(id)
+
+            expect(returned).toEqual(0)
+            expect(returned).toBeFalsy()
+        })
+
+        it("should return error when passed wrong id", async () => {
+            const id = 1
+            const returned = await Promotions.remove(id)
+
+            expect(returned).toEqual(1)
+            expect(returned).toBeTruthy()
         })
     })
 })
