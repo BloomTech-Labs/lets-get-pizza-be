07884e186c88a009ab09c44267e6906ef50fa462
Buddy Agyin <agyin3@gmail.com>
2020-04-23 21:56:59 -0600

    add reviews tests and uncomment other tests

--- tests/plza.test.js
+++ tests/plza.test.js
@@ -1,7 +1,7 @@
 //Require the test files.
 
-// require('./components/locations.js')
-// require('./components/users.js')
-// require('./components/events.js')
-// require('./components/promotions.js')
+require('./components/locations.js')
+require('./components/users.js')
+require('./components/events.js')
+require('./components/promotions.js')
 require('./components/reviews')
