a12344d45067e6bb30f27581cafb786d8383d0c0
Buddy Agyin <agyin3@gmail.com>
2020-04-23 21:57:15 -0600

    add tests for update and remove models

--- tests/components/reviews.js
+++ tests/components/reviews.js
@@ -6,6 +6,7 @@ const Reviews = require('../../components/reviews/reviews-model')
 // Reviews Dummy Data
 const good_review_details = {user_id: 1, location_id: 1, rating: 3}
 const bad_review_details = {user_id: null, location_id: null, review: "Hello"}
+const updated_review_details = {review_title: "11/10 would recommend", review_text: "You haven't had pizza until you've tried theres"}
 
 // User Login Credentials
 const user_creds = {username: "testing", password: "pizzaTime", email: "test@username.com"}
@@ -58,6 +59,57 @@ describe("Reviews Tests", () => {
 
             expect(returnedReviews).toHaveLength(1)
             expect(returnedReviews[0]).toMatchObject(good_review_details)
+        })
+    })
+
+    describe("Update Model", () => {
+        beforeEach(async () => {
+            await db('reviews').insert(good_review_details)
+        })
+
+        it("should return an error when given wrong id", async () => {
+            const id = 5
+            const failedUpdate = await Reviews.update(updated_review_details, id)
+            const failedMatchingObject = {message: 'Undefined binding(s) detected when compiling FIRST. Undefined column(s): [id] query: select * from "reviews" where "id" = ? limit ?'}
+            
+            expect(failedUpdate).not.toBeUndefined()
+            expect(failedUpdate).toMatchObject(failedMatchingObject)
+        })
+
+        it("should return updated review object", async () => {
+            const id = 1
+            const successfulUpdate = await Reviews.update(updated_review_details, id)
+
+            expect(successfulUpdate).not.toBeUndefined()
+            expect(successfulUpdate).toMatchObject(updated_review_details)
+        })
+    })
+
+    describe("Remove Model", () => {
+        beforeEach(async () => {
+            await db('reviews').insert(good_review_details)
+        })
+
+        it("should return 0 when given incorrect id and return reviews", async () => {
+            const id = 9
+            const failedDelete = await Reviews.remove(id)
+            const returnedReviews = await Reviews.find()
+
+            expect(failedDelete).toBe(0)
+            expect(failedDelete).not.toBe(1)
+            expect(failedDelete).toBeFalsy()
+            expect(returnedReviews).toHaveLength(1)
+        })
+
+        it("should return 1 when given correct id and array of no reviews", async () => {
+            const id = 1
+            const successfulDelete = await Reviews.remove(id)
+            const returnedReviews = await Reviews.find()
+
+            expect(successfulDelete).toBe(1)
+            expect(successfulDelete).toBeTruthy()
+            expect(successfulDelete).not.toBe(0)
+            expect(returnedReviews).toHaveLength(0)
         })
     })
 })
