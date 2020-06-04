e878ed172a83e13536890e8d89432ad39a756db3
Buddy Agyin <agyin3@gmail.com>
2020-06-03 21:19:18 -0600

    add route invite friend and update response

--- components/events/events-router.js
+++ components/events/events-router.js
@@ -70,10 +70,19 @@ router.delete("/:id", (req, res) => {
 
 router.get("/users/:id", (req, res) => {
   let user_id = req.params.id;
-  Events.findBy({ user_id })
-    .then((event) => {
-      res.json(event);
-    })
+
+  // use Promise.all to make two simultaneous queries
+  // Events.findBy returns events created by user
+  // Events.findInvitedEvents returns events a 
+  // user is invited to
+
+  Promise.all([
+    Events.findBy({ user_id }),
+    Events.findInvitedEvents({invitee_user_id: user_id})
+  ])
+  .then(([created, invited]) => {
+    res.status(200).json({createdEvents: created, invitedEvents: invited})
+  })
     .catch((err) => {
       res.status(500).json(err);
     });
@@ -88,6 +97,42 @@ router.get("/locations/:id", (req, res) => {
     .catch((err) => {
       res.status(500).json(err);
       console.log(err);
+    })
+})
+
+// Invite a user to an event
+// Body must include the following
+// inviter_user_id, invitee_user_id
+router.post("/:id/invite", (req, res) => {
+  let { id } = req.params
+  let invite = {
+    event_id: parseInt(id),
+    ...req.body
+  }
+
+  Events.inviteFriend(invite)
+    .then(invited => {
+      res.status(201).json(invited)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+// Update response for event invite
+// Body should only include
+// response
+router.put("/:id/invite/:invite_id", (req, res) => {
+  let id = parseInt(req.params.invite_id)
+  let updates = {
+    response: req.body.response.toLowerCase() 
+  }
+  Events.updateInvite(updates, id)
+    .then(updates => {
+      res.status(200).json({invited: updates})
+    })
+    .catch(err => {
+      res.status(500).json(err)
     })
 })
 
